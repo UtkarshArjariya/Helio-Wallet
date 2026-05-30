299e71efbd257fb07c4a71ae8bdca10e12a6b169
UtkarshArjariya <utkarsharjariya188@gmail.com>
2026-05-30 18:18:19 +0530

    remove unused environment variables for mainnet RPC in vite-env.d.ts

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -2,8 +2,6 @@
 
 interface ImportMetaEnv {
   readonly VITE_HELIO_MAINNET_RPC_PRIMARY_URL?: string;
-  readonly VITE_HELIO_MAINNET_RPC_PRIMARY_WS_URL?: string;
-  readonly VITE_HELIO_MAINNET_RPC_FALLBACK_URL?: string;
   readonly VITE_HELIO_DEVNET_RPC_PRIMARY_URL?: string;
   readonly VITE_HELIO_JUPITER_API_KEY?: string;
   readonly VITE_HELIO_JUPITER_API_BASE_URL?: string;
